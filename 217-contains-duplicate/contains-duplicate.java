912dfdce4615c22ae90875969466fc3745452537
Vuyisanani <mbebevuyisanani@gmail.com>
2025-11-06 18:42:23 +0200

    Time: 25 ms (6.49%) | Memory: 76.8 MB (5.20%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution 
+{
+    // will traverse the array compare the current value to the previous value if whether of not they are equal
+    public boolean containsDuplicate(int[] nums) 
+    {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++)
+        {
+            if (nums[i] == nums[i + 1])
+            return true;
+        }
+        return false;
+    }
+}
